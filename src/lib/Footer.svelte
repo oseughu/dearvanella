d8b932d5cf19ee732e2a22affb946d6367d9a540
Ose Ughu <zinjmyster10@gmail.com>
2022-01-01 09:20:50 +0100

    remove copyright word from footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,8 +2,8 @@
   let currentYear = new Date().getFullYear()
 </script>
 
-<footer class="p-4 footer bg-base-300 text-base-content footer-center">
-  Copyright &copy; {currentYear} Vanella Macaulay
+<footer class="p-4 footer bg-base-300 text-base-content footer-center"
+  >&copy; {currentYear} Vanella Macaulay
 </footer>
 
 <style>
